4f19598f893ddd66f5304a72202151fd39724b5c
BCITDevonbui <dbui17@my.bcit.ca>
2021-11-03 16:13:50 -0700

    Devon Added Login

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var  firebaseConfig = {
+    apiKey: "AIzaSyAE_pJZ6yzOPpp9Ci94NjuEtxReFkYACKw",
+   authDomain: "bby21-3c9da.firebaseapp.com",
+   projectId: "bby21-3c9da",
+   storageBucket: "bby21-3c9da.appspot.com",
+   messagingSenderId: "1092942334504",
+   appId: "1:1092942334504:web:06af4d70e6a76fd102031d"
+
+ };
+
+
+ const app = firebase.initializeApp(firebaseConfig);
+
